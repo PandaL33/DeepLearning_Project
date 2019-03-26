42323bc06a7557f57ab1db05912e0e95c1fb88ed
liguohuang <liguohuang@ut.cn>
2019-03-26 08:25:55 +0800

    Add Training

--- TensorFlowLearning/TensorFlowLearning/Object_Detection/xml_to_csv.py
+++ TensorFlowLearning/TensorFlowLearning/Object_Detection/xml_to_csv.py
@@ -7,19 +7,19 @@ import xml.etree.ElementTree as ET
 
 def xml_to_csv(path):
     xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
+    xml_path=path + '\\*.xml'
+    for xml_file in glob.glob(xml_path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
+            value = (root.find('path').text,
+                    int(root.find('size').find('width').text),
+                    int(root.find('size').find('width').text),
+                    member.find('name').text,
+                    int(member.find('bndbox').find('xmin').text),
+                    int(member.find('bndbox').find('ymin').text),
+                    int(member.find('bndbox').find('xmax').text),
+                    int(member.find('bndbox').find('ymax').text))
             xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
@@ -27,7 +27,7 @@ def xml_to_csv(path):
 
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'annotations')
+    image_path = os.path.join(os.getcwd(), 'train\\annotations')
     xml_df = xml_to_csv(image_path)
     xml_df.to_csv('road_signs_labels.csv', index=None)
     print('Successfully converted xml to csv.')
