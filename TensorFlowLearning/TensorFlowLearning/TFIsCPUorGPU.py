1bdc605c38a2b106d17cae53cedf398451c83b08
pandal33 <pandal33@outlook.com>
2019-03-29 12:28:08 +0600

    Update InceptionV3

---
+++ TensorFlowLearning/TensorFlowLearning/TFIsCPUorGPU.py
@@ -0,0 +1,14 @@
+import os
+from tensorflow.python.client import device_lib
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "99"
+
+if __name__ == "__main__":
+    print(device_lib.list_local_devices())
+
+# import numpy
+# import tensorflow as tf
+# a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+# b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+# c = tf.matmul(a, b)
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# print(sess.run(c))
